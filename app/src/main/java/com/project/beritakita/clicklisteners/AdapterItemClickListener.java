f4313cf21aa1443afd6231c0625b039dda37fe4c
Kartikaaghs <kartikaaghnisafitri@gmail.com>
2022-07-15 08:52:25 +0700

    BeritaKita

---
+++ app/src/main/java/com/project/beritakita/clicklisteners/AdapterItemClickListener.java
@@ -0,0 +1,9 @@
+package com.project.beritakita.clicklisteners;
+
+import com.project.beritakita.model.News;
+
+public interface AdapterItemClickListener {
+
+    void onNewsItemClick(News news);
+
+}
---
+++ app/src/main/java/com/project/beritakita/clicklisteners/NewsDialogClickListeners.java
@@ -0,0 +1,8 @@
+package com.project.beritakita.clicklisteners;
+
+public interface NewsDialogClickListeners {
+
+    void onGotoWebSiteClick(String url);
+    void onDismissClick();
+
+}
